6a22d70b9a5b92fc2265c3c2d58fbdfef93b7ce9
Alexander Kurth <alexander.kurth@etu.unilim.fr>
2018-06-24 15:06:46 +0200

    Debut de la gestion du jeu dans le main

--- src/main/MenuBatailleNavale.java
+++ src/main/MenuBatailleNavale.java
@@ -1,27 +1,37 @@
 package main;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-import fenetreGrille.BoutonBateaux;
+import divers.BoutonSpecial;
 import fenetreGrille.Grille;
+import joueur.Joueur;
 
 
 public class MenuBatailleNavale extends JFrame{
 	
-	private Grille grille ;
-	private BoutonBateaux boutons;
+	private BoutonSpecial changementTour;
 	
-	public MenuBatailleNavale(){		
-		this.grille = new Grille(11,11); 
-		this.add(grille,BorderLayout.CENTER);
-		/*
-		this.boutons = new BoutonBateaux();
-		this.add(boutons,BorderLayout.WEST);
-		*/
+	private JLabel nomJoueur;
+	
+	private Grille grilleJoueur1 ;
+	private Grille grilleJoueur2 ;
+
+	
+	private Joueur joueur1 = new Joueur("Carlos",grilleJoueur1,1);
+	private Joueur joueur2 = new Joueur("Kukuss",grilleJoueur2,2);
+	
+	public MenuBatailleNavale(){
 		
+		this.changementTour = new BoutonSpecial("Bouton Special",0);
+		
+		
+		this.grilleJoueur1 = joueur1.getGrilleJoueur(); 
+		this.nomJoueur = new JLabel("");
+		this.add(grilleJoueur1,BorderLayout.WEST);
+		this.add(nomJoueur, BorderLayout.NORTH);
 	
 	//parametres de la fenetre
 		this.setTitle("Bataille Navale 54");
@@ -35,6 +45,14 @@ public class MenuBatailleNavale extends JFrame{
 		//Main
 		public static void main(String[] args) {
 			new MenuBatailleNavale();
+		}
+		
+		public boolean estPair(int x) {
+			if(x%2 == 0) {
+				return true;
+			}else {
+				  return false;
+			}
 		}
 
 }
